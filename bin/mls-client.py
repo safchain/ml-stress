bd25c671300386e4cfbe48ff2b176f88804b7009
Sylvain Afchain <safchain@gmail.com>
2014-04-07 18:48:43 +0200

    Fix typo and unit for Throughtput

--- bin/mls-client.py
+++ bin/mls-client.py
@@ -24,18 +24,19 @@ ERROR_PACKET = 3
 class DroneConnectionException(Exception):
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return repr(self.value)
 
 
 class DroneClient(protocol.Protocol):
-    
+
     def connectionMade(self):
         run = self.factory.run
         connection = self.factory.connection
-        
+
         to = connection['to'].split(':')
-        
+
         job_request = drone_pb2.JobRequest()
         job_request.port = int(to[1])
         job_request.block_size = run['block_size']
@@ -47,13 +48,13 @@ class DroneClient(protocol.Protocol):
         job_request.messages_interval = run['messages_interval']
         job_request.messages_percent = run['messages_percent']
         job_request.timeout = run['timeout']
-        
+
         addr = socket.gethostbyname(to[0])
         job_request.ipv4 = struct.unpack("!I", socket.inet_aton(addr))[0]
-        
+
         job_str = job_request.SerializeToString()
         self.transport.write(struct.pack('!I', len(job_str)) + job_str)
-    
+
     def dataReceived(self, data):
         (size, type), data = struct.unpack("!IB", data[:5]), data[5:]
 
@@ -63,10 +64,10 @@ class DroneClient(protocol.Protocol):
             self.intervalReceived(data)
         elif type == ERROR_PACKET:
             self.errorMessageReceived(data)
-            
+
     def errorMessageReceived(self, data):
         connection = self.factory.connection
-        
+
         try:
             job_error = drone_pb2.JobErrorMessage()
             job_error.ParseFromString(data)
@@ -74,15 +75,15 @@ class DroneClient(protocol.Protocol):
             return
 
         print "%s - %s" % (connection['from'], job_error.message)
-          
+
     def intervalReceived(self, data):
         connection = self.factory.connection
-        
+
         self.factory.interval += 1
-        
+
         try:
             job_interval = drone_pb2.JobIntervalResult()
-            job_interval.ParseFromString(data)  
+            job_interval.ParseFromString(data)
         except:
             return
 
@@ -92,25 +93,25 @@ class DroneClient(protocol.Protocol):
                     self.factory.interval,
                     job_interval.total_messages_read,
                     job_interval.total_bytes_read,
-                    job_interval.total_bytes_read / 
-                    (self.factory.run['interval'] * 1024 * 1024),
+                    job_interval.total_bytes_read /
+                    (self.factory.run['interval'] * 1024),
                     job_interval.total_sessions)
-            
+
     def totalReceived(self, data):
         job_result = drone_pb2.JobResult()
         job_result.ParseFromString(data)
-        
-        run_id = self.factory.run['id'] 
-        
+
+        run_id = self.factory.run['id']
+
         # global result
         result = self.factory.results.get(run_id)
         if not result:
             result = {'total_sessions':
                       job_result.total_sessions,
                       'total_drone_results': 1,
-                      'total_bytes_read': 
+                      'total_bytes_read':
                       job_result.total_bytes_read,
-                      'total_messages_read': 
+                      'total_messages_read':
                       job_result.total_messages_read}
             self.factory.results[run_id] = result
         else:
@@ -118,18 +119,18 @@ class DroneClient(protocol.Protocol):
             result['total_drone_results'] += 1
             result['total_bytes_read'] += job_result.total_bytes_read
             result['total_messages_read'] += job_result.total_messages_read
-            
+
         # result per drone
         per_drone = result.get('drones', {})
         per_drone[self.factory.connection['from']] = {
             'total_sessions':
             job_result.total_sessions,
-            'total_bytes_read': 
+            'total_bytes_read':
             job_result.total_bytes_read,
-            'total_messages_read': 
+            'total_messages_read':
             job_result.total_messages_read,
             'total_throughtput_kbs': (
-                job_result.total_bytes_read / 
+                job_result.total_bytes_read /
                 (self.factory.run['seconds'] * 1024))}
         result['drones'] = per_drone
 
@@ -150,24 +151,24 @@ class DroneFactory(protocol.ClientFactory):
         self.results = results
         self.num_jobs = num_jobs
         self.interval = 0
-        
+
     def clientConnectionFailed(self, connector, reason):
         results = self.results.get(self.run['id'])
         if not results:
             raise DroneConnectionException(
                 "Error no result for connection : %s" % self.connection)
-        
-        if(results['total_drone_results'] == self.num_jobs and 
+
+        if(results['total_drone_results'] == self.num_jobs and
            reactor.running):
             reactor.stop()
-    
+
     def clientConnectionLost(self, connector, reason):
         results = self.results.get(self.run['id'])
         if not results:
             raise DroneConnectionException(
                 "Error no result for connection : %s" % self.connection)
-        
-        if(results['total_drone_results'] == self.num_jobs and 
+
+        if(results['total_drone_results'] == self.num_jobs and
            reactor.running):
             reactor.stop()
 
@@ -178,44 +179,44 @@ def usage():
 
 
 def main(argv):
-    
-    try:                                
+
+    try:
         opts, args = getopt.getopt(argv, "r:", ["requests="])
-    except getopt.GetoptError: 
+    except getopt.GetoptError:
         usage()
-            
+
     for opt, arg in opts:
         if opt in ('-r', '--requests'):
-           
+
             with open(arg, 'r') as stream:
                 runs = yaml.load(stream)
-            
+
             results = {}
             for run in runs:
                 num_jobs = len(run['connections'])
                 for connection in run['connections']:
                     f = connection['from'].split(':')
-            
+
                     df = DroneFactory(run,
                                       connection,
                                       results,
                                       num_jobs)
-                    
+
                     reactor.connectTCP(f[0], int(f[1]), df)
-            
+
                 reactor.run()
-                
+
                 for connection in run['connections']:
                     results[run['id']]['total_throughtput_kbs'] = (
-                        results[run['id']]['total_bytes_read'] / 
+                        results[run['id']]['total_bytes_read'] /
                     (run['seconds'] * 1024))
-                
+
             print yaml.safe_dump(results, default_flow_style=False)
-            
+
             break
     else:
         usage()
-    
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
